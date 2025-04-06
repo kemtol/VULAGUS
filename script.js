8f76a8d530e764e624c9dd1dce73e384e00dd0b8
kemtol <mkemalw@gmail.com>
2025-04-06 17:52:06 +0700

    browse sugaluv

--- script.js
+++ script.js
@@ -62,11 +62,11 @@ $(document).ready(async function () {
   // ===============================
   // Tombol browse sugaluv
   // ===============================
-  $("#browse-more-sugalove").click(function (e) {
+$("a#browse-more-sugalove").click(function (e) {
     e.preventDefault();
     $("#landing-screen").addClass("d-none");
     $("#matching-screen").removeClass("d-none");
-  });
+});
 
 
   // ===============================
